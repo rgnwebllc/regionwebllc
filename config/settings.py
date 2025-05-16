f7a1247d1ae2631e83d07372e0735a96a8f8bf5d
anthony.ryan@regionwebllc.com <anthony.chalabis01@gmail.com>
2025-05-15 23:34:19 -0500

    Accident

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env("SECRET_KEY")
 DISCORD_WEBHOOK_URL = env('DISCORD_WEBHOOK_URL')
 DISCORD_LOG_TOKEN = env('DISCORD_LOG_TOKEN')
-# DEBUG = env("DEBUG")
-# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 #Development Hosts
-ALLOWED_HOSTS = ['10.0.0.207', '127.0.0.1']
-DEBUG = True
+# ALLOWED_HOSTS = ['10.0.0.207', '127.0.0.1']
 
 class DiscordLogHandler(logging.Handler):
     def emit(self, record):
