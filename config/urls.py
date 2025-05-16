8691bc1424793a7ba46a1f43c935e935e1732cc7
anthony.ryan@regionwebllc.com <anthony.chalabis01@gmail.com>
2025-05-15 20:48:20 -0500

    Added robots.txt and sitemap.xml

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.admin import custom_admin_site
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', custom_admin_site.urls),
     path('', include('core.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ core/sitemaps.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.8
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['home', 'about', 'features', 'portfolio', 'consultation', 'testemonials']
+
+    def location(self, item):
+        return reverse(item)
