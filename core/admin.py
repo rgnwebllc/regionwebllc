8c12f348db9a6ab5f22318a07031f07382fdc437
rgnwebllc <anthony.ryan@regionwebllc.com>
2025-05-19 17:32:06 -0700

    Updated admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from config.admin_site import custom_admin_site
-from .models import Testimonial
+from .models import *
 
 @admin.register(Testimonial, site=custom_admin_site)
 class TestimonialAdmin(admin.ModelAdmin):
     list_display = ('name', 'role', 'approved', 'created_at')
     list_filter = ('approved', 'created_at')
     search_fields = ('name', 'quote')
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_at')
